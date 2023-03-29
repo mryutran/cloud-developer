c0c735949e1f25c1b4bb9a8b51323564d111cc9b
Phong Tran <YuTran@Yus-iMac.local>
2023-03-29 13:23:26 +0700

    Updated code

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { Router, Request, Response } from 'express';
 
 (async () => {
 
@@ -17,11 +18,21 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
+  app.get("/filteredimage", async (req: Request, res:Response) => {
+    const image_url:string= req.query.image_url
+    //    1. validate the image_url query
+    if(!image_url){
+      return res.status(400).send({ message: 'URL is not valid' });
+    }
+    //    2. call filterImageFromURL(image_url) to filter the image
+     const filteredpath = await filterImageFromURL(image_url)
+    //    3. send the resulting file in the response
+     res.sendFile(filteredpath)
+    //    4. deletes any files on the server on finish of the response
+     res.on("finish" ,() => deleteLocalFiles([filteredpath]))
+  }
+    );
+
   // QUERY PARAMATERS
   //    image_url: URL of a publicly accessible image
   // RETURNS
